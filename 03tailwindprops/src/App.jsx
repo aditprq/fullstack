b2b48614be76502628971ca91122b951b2a618d2
aditprq <143347564+aditprq@users.noreply.github.com>
2024-01-29 19:37:32 +0530

    04bgChanger

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
      <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
      <Card userName="Aditya"  post='CEO'/>
      <Card userName='Adit' myArr={newArr}/>
-     <Card />
+     <Card userName='Suraj Kumar' post ='CEO'/>
     </>
   )
 }
---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+  let discoInterval;
+  function disco() {
+    let colors = ['red', 'blue', 'white', 'green', 'yellow', 'orange', 'purple']
+    let i = 0;
+     discoInterval = setInterval(() => {
+      setColor(colors[i])
+      i = i + 1
+      if (i >= colors.length) {
+        i = 0
+      }
+    }, 500)
+
+  }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'> 
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+        <button onClick={()=>setColor('red')} className='outline-none px-4 py-1 rounded-full shadow-lg text-white' 
+        style={{backgroundColor:'red'}}
+        >Red</button>
+        <button onClick={()=>setColor('blue')} className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+        style={{backgroundColor:'blue'}}
+        >Blue</button>
+        <button onClick={()=>setColor('black')} className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+        style={{backgroundColor:'black'}}
+        >Black</button>
+        <button onClick={()=>setColor('white')} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+        style={{backgroundColor:'white'}}
+        >White</button>
+        <button onClick={()=>disco()} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+        style={{backgroundColor:'white'}}
+        >Disco</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
